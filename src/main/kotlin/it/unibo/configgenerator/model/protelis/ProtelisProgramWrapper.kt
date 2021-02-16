7bb87ea6fe9d5b3ad673ed4e464535801317f0e0
APlacuzzi <andrea.placuzzi@unibo.it>
2021-02-16 18:14:03 +0100

    take in account hidden fields

--- src/main/kotlin/it/unibo/configgenerator/model/protelis/ProtelisProgramWrapper.kt
+++ src/main/kotlin/it/unibo/configgenerator/model/protelis/ProtelisProgramWrapper.kt
@@ -1,8 +1,8 @@
 package it.unibo.configgenerator.model.protelis
 
 import org.protelis.lang.interpreter.ProtelisAST
-import org.protelis.lang.interpreter.impl.Constant
-import org.protelis.lang.interpreter.impl.JvmConstant
+import org.protelis.lang.interpreter.impl.*
+import org.protelis.parser.protelis.Share
 import org.protelis.vm.ExecutionContext
 import org.protelis.vm.ProtelisProgram
 import org.protelis.vm.impl.SimpleProgramImpl
@@ -25,10 +25,18 @@ data class ProtelisProgramWrapper(private val protelisProgram: ProtelisProgram):
 
     fun estimateMips() = flatAst(prog).map { mipsByInstruction(it) }.sum()
 
-    private fun flatAst(ast: ProtelisAST<*>): List<ProtelisAST<*>> = listOf(ast) + ast.branches.flatMap { flatAst(it) }
+    private fun flatAst(ast: ProtelisAST<*>): Sequence<ProtelisAST<*>> = sequenceOf(ast) +
+        ast.branches.flatMap { flatAst(it) } +
+        when(ast) {
+            is Invoke -> flatAst(ast.leftExpression)
+            is FunctionCall -> flatAst(ast.functionDefinition.body)
+            is ShareCall<*, *> -> ast.yieldExpression.transform { flatAst(it!!) }.or(emptySequence())
+            is If<*> -> flatAst(ast.conditionExpression) + flatAst(ast.thenExpression) + flatAst(ast.elseExpression)
+            else -> emptySequence()
+        }
 
-    private fun mipsByInstruction(ast: ProtelisAST<*>) = when (ast) {
-        is Constant<*>, is JvmConstant -> 1.0
+    private fun mipsByInstruction(ast: ProtelisAST<*>): Double = when (ast) {
+        is Constant<*>, is JvmConstant, is Env, is Self, is FunctionCall -> 1.0
         else -> 50.0
     }
 }
