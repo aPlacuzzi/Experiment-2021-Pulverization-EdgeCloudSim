fbbb186e8c1eae9c3702a2b2b431f3fa35d9fa0a
APlacuzzi <andrea.placuzzi@unibo.it>
2021-02-17 12:59:46 +0100

    add hashing code path factory

--- src/main/kotlin/it/unibo/configgenerator/model/protelis/DummyExecutionContext.kt
+++ src/main/kotlin/it/unibo/configgenerator/model/protelis/DummyExecutionContext.kt
@@ -1,14 +1,16 @@
 package it.unibo.configgenerator.model.protelis
 
+import com.google.common.hash.Hashing
 import org.protelis.lang.datatype.DeviceUID
 import org.protelis.lang.datatype.impl.StringUID
 import org.protelis.vm.NetworkManager
 import org.protelis.vm.impl.AbstractExecutionContext
+import org.protelis.vm.impl.HashingCodePathFactory
 import org.protelis.vm.impl.SimpleExecutionEnvironment
 
 class DummyExecutionContext(
     netmgr: NetworkManager,
-): AbstractExecutionContext<DummyExecutionContext>(SimpleExecutionEnvironment(), netmgr) {
+): AbstractExecutionContext<DummyExecutionContext>(SimpleExecutionEnvironment(), netmgr, HashingCodePathFactory(Hashing.sha512())) {
 
     override fun getCurrentTime(): Number = 0
 
