ea34545001d2c3eedce7300bcb85c8dfff49a2f7
APlacuzzi <andrea.placuzzi@unibo.it>
2021-02-17 12:59:07 +0100

    encode key in Base64

--- src/main/kotlin/it/unibo/configgenerator/model/protelis/DummyNetworkManager.kt
+++ src/main/kotlin/it/unibo/configgenerator/model/protelis/DummyNetworkManager.kt
@@ -5,18 +5,21 @@ import org.protelis.lang.datatype.DeviceUID
 import org.protelis.vm.CodePath
 import org.protelis.vm.NetworkManager
 import org.nustaq.serialization.FSTConfiguration
+import org.protelis.vm.impl.HashingCodePathFactory
+import java.util.*
 
 class DummyNetworkManager: NetworkManager {
 
-    private val conf = FSTConfiguration.createDefaultConfiguration()
+    private val fst = FSTConfiguration.createDefaultConfiguration()
     private val jackson = ObjectMapper()
     private var msgSize: Int = 0
 
     override fun getNeighborState(): MutableMap<DeviceUID, MutableMap<CodePath, Any>> =
         emptyMap<DeviceUID, MutableMap<CodePath, Any>>().toMutableMap()
 
-    override fun shareState(toSend: MutableMap<CodePath, Any>?) {
-        msgSize = jackson.writeValueAsBytes(toSend).size //conf.asByteArray(jackson.writeValueAsString(toSend)).size
+    override fun shareState(toSend: MutableMap<CodePath, Any>) {
+        val json = jackson.writeValueAsString(toSend.mapKeys { (k, _) -> Base64.getEncoder().encode((k as HashingCodePathFactory.HashingCodePath).hash) })
+        msgSize = fst.asByteArray(json).size
     }
 
     fun getMsgSize() = msgSize
