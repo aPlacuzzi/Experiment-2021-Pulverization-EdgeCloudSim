5fce4ebdacd7941629a6c8a68c14a3624f6816ab
APlacuzzi <andrea.placuzzi@unibo.it>
2021-02-26 14:46:37 +0100

    generate all charts for selected config

--- matlab/plotResults.m
+++ matlab/plotResults.m
@@ -3,11 +3,11 @@ function [] = plotResults(baseDir, folderNum, baseFileName, withError, outputDir
     
     plotAvgNetworkDelay(baseDir, folderNum, baseFileName, withError, outputDir);
     
-    %plotAvgProcessingTime(baseDir, baseFileName, withError, outputDir);
+    plotAvgProcessingTime(baseDir, baseFileName, withError, outputDir);
     
-    %plotAvgServiceTime(baseDir, baseFileName, withError, outputDir);
+    plotAvgServiceTime(baseDir, baseFileName, withError, outputDir);
 	
-	%plotAvgVmUtilization(baseDir, baseFileName, withError, outputDir);
+	plotAvgVmUtilization(baseDir, baseFileName, withError, outputDir);
 	
-	%plotTaskFailureReason(baseDir, baseFileName, withError, outputDir);
+	plotTaskFailureReason(baseDir, baseFileName, withError, outputDir);
 end
