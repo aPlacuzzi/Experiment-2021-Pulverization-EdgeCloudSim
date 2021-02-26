2aa3deb135937cc73a933f93b9d78ccf26c7a2ec
APlacuzzi <andrea.placuzzi@unibo.it>
2021-02-26 15:32:59 +0100

    fix parameters order

--- matlab/plotTaskFailureReason.m
+++ matlab/plotTaskFailureReason.m
@@ -4,9 +4,9 @@ function [] = plotTaskFailureReason(baseDir, folderNum, baseFileName, withError,
     
     plotGenericResult(1, 11, 'Failed Task due to Mobility (%)', 'ALL_APPS', 'percentage_for_all', withError, baseDir, folderNum, strcat(baseFileName, 'Failed_Task_due_to_Mobility'), outputDir);
     
-    plotGenericResult(5, 5, 'Failed Tasks due to WLAN failure (%)', 'ALL_APPS', 'percentage_for_all', withError, folderNum, baseDir, strcat(baseFileName, 'Failed_Task_due_to_WLAN'), outputDir);
+    plotGenericResult(5, 5, 'Failed Tasks due to WLAN failure (%)', 'ALL_APPS', 'percentage_for_all', withError, baseDir, folderNum, strcat(baseFileName, 'Failed_Task_due_to_WLAN'), outputDir);
     
-    plotGenericResult(5, 7, 'Failed Tasks due to WAN failure (%)', 'ALL_APPS', 'percentage_for_all', withError, folderNum, baseDir, strcat(baseFileName, 'Failed_Task_due_to_WAN'), outputDir);
+    plotGenericResult(5, 7, 'Failed Tasks due to WAN failure (%)', 'ALL_APPS', 'percentage_for_all', withError, baseDir, folderNum, strcat(baseFileName, 'Failed_Task_due_to_WAN'), outputDir);
 	
 	plotGenericResult(1, 10, 'Failed Task due to VM Capacity (%)', 'ALL_APPS', '', withError, baseDir, folderNum, strcat(baseFileName, 'Failed_Task_due_to_VM_Capacity_Pure'), outputDir);
     
