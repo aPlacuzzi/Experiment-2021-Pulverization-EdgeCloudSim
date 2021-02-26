2adc06934cc3ce329d63cf5cc9e6ae778db21b8a
APlacuzzi <andrea.placuzzi@unibo.it>
2021-02-26 14:52:55 +0100

    fix paths

--- plotAll.m
+++ plotAll.m
@@ -1,27 +1,27 @@
 function [] = plotAll()
 	addpath(fullfile('./', 'matlab'));
 	initEnv();
-    plotResults('../output/results/behaviourAndCommunication', 6, 'EaC6_', false, '../output/charts/behaviourAndCommunication');
-    plotResults('../output/results/behaviourWithCommunication', 1, 'EwC1_', false, '../output/charts/behaviourWithCommunication');
-    plotResults('../output/results/onlyCommunication', 1, 'C1_', false, '../output/charts/onlyCommunication');
+    plotResults('./output/results/behaviourAndCommunication', 6, 'EaC6_', false, './output/charts/behaviourAndCommunication');
+    plotResults('./output/results/behaviourWithCommunication', 1, 'EwC1_', false, './output/charts/behaviourWithCommunication');
+    plotResults('./output/results/onlyCommunication', 1, 'C1_', false, './output/charts/onlyCommunication');
 	
-	plotResults('../output/results/behaviourAndCommunication', 6, 'EaC6_withStdv_', true, '../output/charts/behaviourAndCommunication');
-    plotResults('../output/results/behaviourWithCommunication', 1, 'EwC1_withStdv_', true, '../output/charts/behaviourWithCommunication');
-    plotResults('../output/results/onlyCommunication', 1, 'C1_withStdv_', true, '../output/charts/onlyCommunication');
+	plotResults('./output/results/behaviourAndCommunication', 6, 'EaC6_withStdv_', true, './output/charts/behaviourAndCommunication');
+    plotResults('./output/results/behaviourWithCommunication', 1, 'EwC1_withStdv_', true, './output/charts/behaviourWithCommunication');
+    plotResults('./output/results/onlyCommunication', 1, 'C1_withStdv_', true, './output/charts/onlyCommunication');
 	
-	plotResults('../output/results/behaviourAndCommunication', 12, 'EaC12_', false, '../output/charts/behaviourAndCommunication');
-    plotResults('../output/results/behaviourWithCommunication', 2, 'EwC2_', false, '../output/charts/behaviourWithCommunication');
-    plotResults('../output/results/onlyCommunication', 2, 'C2_', false, '../output/charts/onlyCommunication');
+	plotResults('./output/results/behaviourAndCommunication', 12, 'EaC12_', false, './output/charts/behaviourAndCommunication');
+    plotResults('./output/results/behaviourWithCommunication', 2, 'EwC2_', false, './output/charts/behaviourWithCommunication');
+    plotResults('./output/results/onlyCommunication', 2, 'C2_', false, './output/charts/onlyCommunication');
 	
-	plotResults('../output/results/behaviourAndCommunication', 12, 'EaC12_withStdv_', true, '../output/charts/behaviourAndCommunication');
-    plotResults('../output/results/behaviourWithCommunication', 2, 'EwC2_withStdv_', true, '../output/charts/behaviourWithCommunication');
-    plotResults('../output/results/onlyCommunication', 2, 'C2_withStdv_', true, '../output/charts/onlyCommunication');
+	plotResults('./output/results/behaviourAndCommunication', 12, 'EaC12_withStdv_', true, './output/charts/behaviourAndCommunication');
+    plotResults('./output/results/behaviourWithCommunication', 2, 'EwC2_withStdv_', true, './output/charts/behaviourWithCommunication');
+    plotResults('./output/results/onlyCommunication', 2, 'C2_withStdv_', true, './output/charts/onlyCommunication');
 	
-	plotResults('../output/results/behaviourAndCommunication', 18, 'EaC18_', false, '../output/charts/behaviourAndCommunication');
-    plotResults('../output/results/behaviourWithCommunication', 3, 'EwC3_', false, '../output/charts/behaviourWithCommunication');
-    plotResults('../output/results/onlyCommunication', 3, 'C3_', false, '../output/charts/onlyCommunication');
+	plotResults('./output/results/behaviourAndCommunication', 18, 'EaC18_', false, './output/charts/behaviourAndCommunication');
+    plotResults('./output/results/behaviourWithCommunication', 3, 'EwC3_', false, './output/charts/behaviourWithCommunication');
+    plotResults('./output/results/onlyCommunication', 3, 'C3_', false, './output/charts/onlyCommunication');
 	
-	plotResults('../output/results/behaviourAndCommunication', 18, 'EaC18_withStdv_', true, '../output/charts/behaviourAndCommunication');
-    plotResults('../output/results/behaviourWithCommunication', 3, 'EwC3_withStdv_', true, '../output/charts/behaviourWithCommunication');
-    plotResults('../output/results/onlyCommunication', 3, 'C3_withStdv_', true, '../output/charts/onlyCommunication');
+	plotResults('./output/results/behaviourAndCommunication', 18, 'EaC18_withStdv_', true, './output/charts/behaviourAndCommunication');
+    plotResults('./output/results/behaviourWithCommunication', 3, 'EwC3_withStdv_', true, './output/charts/behaviourWithCommunication');
+    plotResults('./output/results/onlyCommunication', 3, 'C3_withStdv_', true, './output/charts/onlyCommunication');
 end
