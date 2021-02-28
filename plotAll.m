8eb256b2ba356826e0d11c4547b9d8f54f372c65
APlacuzzi <andrea.placuzzi@unibo.it>
2021-02-28 10:00:11 +0100

    fix charts name

--- plotAll.m
+++ plotAll.m
@@ -5,15 +5,15 @@ function [] = plotAll()
 	mkdir('./output/charts/behaviourWithCommunication');
 	mkdir('./output/charts/onlyCommunication');
 	for i=0:24
-		plotResults('./output/results/behaviourAndCommunication', i, strcat('EaC', int2str(1), '_'), false, './output/charts/behaviourAndCommunication');
-		plotResults('./output/results/behaviourAndCommunication', i, strcat('EaC', int2str(1), '_withStdv_'), true, './output/charts/behaviourAndCommunication');
+		plotResults('./output/results/behaviourAndCommunication', i, strcat('EaC', int2str(i), '_'), false, './output/charts/behaviourAndCommunication');
+		plotResults('./output/results/behaviourAndCommunication', i, strcat('EaC', int2str(i), '_withStdv_'), true, './output/charts/behaviourAndCommunication');
 	end
 	for i=0:4
-		plotResults('./output/results/behaviourWithCommunication', i, strcat('EwC', int2str(1), '_'), false, './output/charts/behaviourWithCommunication');
-		plotResults('./output/results/behaviourWithCommunication', i, strcat('EwC', int2str(1), '_withStdv_'), true, './output/charts/behaviourWithCommunication');
+		plotResults('./output/results/behaviourWithCommunication', i, strcat('EwC', int2str(i), '_'), false, './output/charts/behaviourWithCommunication');
+		plotResults('./output/results/behaviourWithCommunication', i, strcat('EwC', int2str(i), '_withStdv_'), true, './output/charts/behaviourWithCommunication');
 	end
 	for i=0:4
-		plotResults('./output/results/onlyCommunication', i, strcat('C', int2str(1), '_'), false, './output/charts/onlyCommunication');
-		plotResults('./output/results/onlyCommunication', i, strcat('C', int2str(1), '_withStdv_'), true, './output/charts/onlyCommunication');
+		plotResults('./output/results/onlyCommunication', i, strcat('C', int2str(i), '_'), false, './output/charts/onlyCommunication');
+		plotResults('./output/results/onlyCommunication', i, strcat('C', int2str(i), '_withStdv_'), true, './output/charts/onlyCommunication');
 	end
 end
